054accd590d7e6bc19ff3e2e56446a23cb15b652
Prakash <prakashs@dckap.com>
2025-02-18 12:10:21 +0530

    Adobe Event

---
+++ actions/releaseNotification/orderSuccess.js
@@ -0,0 +1,86 @@
+const fetch = require('node-fetch');
+const stateLib = require('@adobe/aio-lib-state');
+
+const oAuthTokenUrl = "https://dev-commquotes-apiservices.heritagedesignservices.com/oauth/token";
+const orderSubUrl = "https://dev-commquotes-apiservices.heritagedesignservices.com/v1/quote/hplusordersub";
+
+
+async function getStoredToken(){
+    // init with implicit I/O Runtime credentials, default region is 'amer'.
+    const state = await stateLib.init();
+
+    // get
+    const res = await state.get('token') // res = { value, expiration }
+    const value = res.value
+
+    if(value){
+        return value;
+    }else{
+        return null;
+    }
+}
+async function getNewToken(client_id,client_secrect){
+    var options = {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            "grant_type":"client_credentials",
+            "client_id":client_id,
+            "client_screct":client_secrect
+        })
+    }
+
+    const response = await fetch(oAuthTokenUrl, options);
+
+    if(response.ok){
+        const tokenData = response.json();
+        await state.put('token',token.token, { ttl: (token.expires_in - 60)});
+        return token.token;
+    }
+
+    return null;
+
+}
+async function main (params) {
+    return {
+        statusCode: 200,
+        body:{
+            data: params
+        }
+    }
+    var token = getStoredToken();
+    var isNewToken = false;
+    if(!token){
+        token = getNewToken(params.client_id,params.client_secrect);
+        if(!token){
+            return {
+                statusCode: 400,
+                body:{
+                    message:"unable to fetch token"
+                }
+            };
+        }else{
+            return {
+                statusCode: 200,
+                body:{
+                    message:"success"
+                }
+            };
+        }
+        isNewToken = true;
+    }
+
+    // var options = {
+    //     method: 'POST',
+    //     headers: {'Content-Type': 'application/json'},
+    //     body: JSON.stringify({
+    //         {"magentoOrderId":"378896","transactionId":"a5843cf8-b096-4081-825c-85c4b7875248","status":"success","message":"Order is Created with Web Reference ID 111000000094"}
+    //     })
+    // }
+
+    // const tokenData = await fetch(orderSubUrl, options);
+    // const token = tokenData.json();
+
+
+}
+exports.main = main;
